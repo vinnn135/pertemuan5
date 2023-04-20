46f8910e0d9cbf76b583cb2fb1aa95dbbf2e8c4b
Mayske <takasihaengmayske@gmail.com>
2023-04-20 08:51:41 +0800

    pertemuan5

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,96 +1,99 @@
 function Card() {
-    return (
-      <>
-        <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-blue-500 to-purple-600">
-          <div class="backdrop-blur-md w-1/2 bg-white bg-opacity-25 rounded-lg shadow-2xl p-8 m-4">
-            <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">
-              Registrasi
-            </h1>
-            <form action="/" method="post">
-              <div class="flex flex-col mb-4">
-                <label
-                  class="mb-2 font-bold text-lg text-gray-900"
-                  for="nama"
-                >
-                  Nama
-                </label>
-                <input
-                  class="border py-2 px-3 text-grey-800"
-                  type="text"
-                  name="nama"
-                  id="nama"
-                />
-              </div>
-              <div class="flex flex-col mb-4">
-                <label
-                  class="mb-2 font-bold text-lg text-gray-900"
-                  for="gender"
-                >
-                  Gender
-                </label>
-                <div>
-                  <input
-                    class="border py-2 text-grey-800"
-                    type="radio"
-                    name="gender"
-                    id="pria"
-                  />
-                  <label
-                  class="mb-2 text-gray-900"
-                  for="pria"
-                  > Pria</label>
-                </div>
-                <div>
-                  <input
-                    class="border py-2 text-grey-800"
-                    type="radio"
-                    name="gender"
-                    id="wanita"
-                  />
-                  <label
-                  class="mb-2 text-gray-900"
-                  for="wanita"
-                  > Wanita</label>
-                </div>
-              </div>
-              <div class="flex flex-col mb-4">
-                <label
-                  class="mb-2 font-bold text-lg text-gray-900"
-                  for="agama"
-                >
-                  Agama
-                </label>
-                <select
-                  class="border py-2 px-3 text-grey-800"
-                  name="agama"
-                  id="agama"
-                >
-                  <option value="Protestan">Kristen Protestan</option>
-                  <option value="Katholik">Katholik</option>
-                  <option value="Islam">Islam</option>
-                  <option value="Hindu">Hindu</option>
-                  <option value="Budha">Budha</option>
-                  <option value="Kepercayaan">Aliran Kepercayaan</option>
-                </select>
-              </div>
-              <button
-                class="block bg-blue-600 hover:bg-blue-00 text-white uppercase text-lg mx-auto p-4 rounded"
-                type="submit"
+  return (
+    <>
+      <div class="flex justify-center items-center h-screen w-full bg-gradient-to-r from-grey-100 to-grey-600">
+        <div class="w-1/2 bg-white rounded-lg shadow-2xl p-8 m-4">
+          <h1 class="block w-full text-center text-gray-800 text-2xl font-bold mb-6">
+            Registrasi Mahasiswa
+          </h1>
+          <form action="/" method="post">
+            <div class="flex flex-col mb-4">
+              <label
+                class="mb-2 font-bold text-lg text-gray-900"
+                for="nama"
               >
-                Buat Akun
-              </button>
-            </form>
-            <a
-              class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:text-gray-900"
-              href="/"
+                Nama
+              </label>
+              <input
+                class="border py-2 px-3 text-grey-800"
+                type="text"
+                name="nama"
+                id="nama"
+              />
+            </div>
+            <div class="flex flex-col mb-4">
+              <label
+                class="mb-2 font-bold text-lg text-gray-900"
+                for="nama"
+              >
+                NIM
+              </label>
+              <input
+                class="border py-2 px-3 text-grey-800"
+                type="number"
+                name="nama"
+                id="nama"
+              />
+            </div>
+            <div class="flex flex-col mb-4">
+              <label
+                class="mb-2 font-bold text-lg text-gray-900"
+                for="alamat"
+              >
+                Alamat
+              </label>
+              <input
+                class="border py-2 px-3 text-grey-800"
+                type="text"
+                name="alamat"
+                id="alamat"
+              />
+            </div>
+            <div class="flex flex-col mb-4">
+              <label
+                class="mb-2 font-bold text-lg text-gray-900"
+                for="nama"
+              >
+                Umur
+              </label>
+              <input
+                class="border py-2 px-3 text-grey-800"
+                type="number"
+                name="years"
+                id="years"
+              />
+            </div>
+            <div class="flex flex-col mb-4">
+              <label
+                class="mb-2 font-bold text-lg text-gray-900"
+                for="nama"
+              >
+                Jenis Kelamin
+              </label>
+              <input
+                class="border py-2 px-3 text-grey-800"
+                type=""
+                name=""
+                id="nama"
+              />
+            </div>
+            <button
+              class="block bg-orange-400 hover:bg-orange-600 text-white uppercase text-lg mx-auto p-4 rounded"
+              type="submit"
             >
-              Telah memiliki Akun? login disini
-            </a>
-          </div>
+              Buat Akun
+            </button>
+          </form>
+          <a
+            class="block w-full text-center no-underline mt-4 text-sm text-gray-700 hover:text-gray-900"
+            href="/"
+          >
+            Telah memiliki Akun?
+          </a>
         </div>
-      </>
-    );
-  }
-  
-  export default Card;
-  
+      </div>
+    </>
+  );
+}
+
+export default Card;
